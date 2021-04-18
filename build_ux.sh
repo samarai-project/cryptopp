cbd24681e369f5b12814565d2a6cfa01455881d6
Jiri Kudrna <jiri.kudrna@samarai-project.org>
2021-04-18 16:35:50 +0800

    build script update

--- build_ux.sh
+++ build_ux.sh
@@ -1,9 +1,7 @@
-# Note: unit tests fail to compile but the lib is compiling
 clear
 rm -r build
 mkdir -p build
 cd build
-# Ninja doesn't seem to work
-cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_C_COMPILER=clang -D CMAKE_CXX_COMPILER=clang .. 
+cmake -D CMAKE_BUILD_TYPE=Release .. 
 cmake --build ./
 cd ..
